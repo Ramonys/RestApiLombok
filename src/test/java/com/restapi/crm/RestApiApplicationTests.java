b8db515286974dc27a9dda1be576720e223a5848
ramonys <ramonys1@hotmail.com>
2021-02-17 23:46:38 -0300

    first commit

---
+++ src/test/java/com/restapi/crm/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
